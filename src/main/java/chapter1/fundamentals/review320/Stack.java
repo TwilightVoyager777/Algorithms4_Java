d20de595efc4ee7784c5c9a9341c9d5a2749c0e6
Klein88 <1440984389@qq.com>
2025-03-20 16:47:39 +0800

    w1d7

---
+++ src/main/java/chapter1/fundamentals/review320/Stack.java
@@ -0,0 +1,44 @@
+package chapter1.fundamentals.review320;
+
+import java.util.NoSuchElementException;
+
+public class Stack<Item> {
+    private Node first;
+    private int size;
+
+    public class Node {
+        Item item;
+        Node next;
+    }
+
+    public Stack() {
+        first = null;
+        size = 0;
+    }
+
+    public void push(Item item) {
+        Node oldFirst = first;
+        first = new Node();
+        first.item = item;
+        first.next = oldFirst;
+        size++;
+    }
+
+    public Item pop(){
+        if (isEmpty()) {
+            throw new NoSuchElementException("栈空");
+        }
+        Item item = first.item;
+        first = first.next;
+        size--;
+        return item;
+    }
+
+    public boolean isEmpty(){
+        return first == null;
+    }
+
+    public int size() {
+        return size;
+    }
+}
